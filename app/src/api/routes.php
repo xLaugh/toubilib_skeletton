7a1c9b4a42c7d778a20d4291c57b8d34198fa02e
Riole88 <boullemanitou@gmail.com>
2025-10-07 15:39:00 +0200

    Nommage route

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -16,28 +16,28 @@ use toubilib\api\middleware\ValidateRDVMiddleware;
 return function( \Slim\App $app):\Slim\App {
 
     // Route pour lister tous les praticiens
-    $app->get('/praticiens', ListePraticiensAction::class);
+    $app->get('/praticiens', ListePraticiensAction::class)->setName("praticiens.all");
 
     //Route pour liser le rdv correspondant à l'id rentré
-    $app->get('/rdvs/{id}', GetRdvById::class);
+    $app->get('/rdvs/{id}', GetRdvById::class)->setName("rdvs.id");
 
     // Route pour obtenir les détails d'un praticien par son ID
-    $app->get('/praticiens/{id}', DetailPraticienAction::class);
+    $app->get('/praticiens/{id}', DetailPraticienAction::class)->setName("praticien.id");
 
     // Route pour lister les créneaux occupés d'un praticien sur une période
-    $app->get('/praticiens/{id}/rdvs', ListerCreneauxOccupesAction::class);
+    $app->get('/praticiens/{id}/rdvs', ListerCreneauxOccupesAction::class)->setName("praticien.crenaux");
 
     // Agenda praticien
-    $app->get('/praticiens/{id}/agenda', AgendaPraticienAction::class);
+    $app->get('/praticiens/{id}/agenda', AgendaPraticienAction::class)->setName("praticien.aganda");
 
     // Annulation de rendez-vous
-    $app->post('/rdvs/{id}/annuler', AnnulerRDVAction::class);
+    $app->post('/rdvs/{id}/annuler', AnnulerRDVAction::class)->setName("rdvs");
 
     // Route pour obtenir un patient selon un id
-    $app->get('/patients/{id}', GetPatientById::class);
+    $app->get('/patients/{id}', GetPatientById::class)->setName("patients.id");
 
     //Route pour ajouter un Rendez-Vous
-    $app->post('/rdvs', CreerRendezVousAction::class)->add(ValidateRDVMiddleware::class);
+    $app->post('/rdvs', CreerRendezVousAction::class)->add(ValidateRDVMiddleware::class)->setName("rdv.ajout");
 
     return $app;
 };
